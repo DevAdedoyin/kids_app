a36fe560bd2aea43d28cfff14d232cd44d69f1d2
Adedoyin <isradeoluwa@gmail.com>
2021-03-16 15:17:44 +0100

    PopMenuButton added

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/to_learn.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key key = const Key('')}) : super(key: key);
@@ -6,8 +7,30 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kids Learning (Offline)'),
+        actions: [
+          PopupMenuButton(
+              itemBuilder: (context) => [
+                    PopupMenuItem(
+                      child: Text('All Scores'),
+                    ),
+                    PopupMenuItem(
+                      child: Text('Settings'),
+                    ),
+                    PopupMenuItem(
+                      child: Text('About'),
+                    ),
+                    PopupMenuItem(
+                      child: Text('Share App'),
+                    ),
+                    PopupMenuItem(
+                      child: Text('More Apps'),
+                    ),
+                  ])
+        ],
+      ),
     );
   }
 }
