7d61f71bedd2e02e5187255616e08c3691571caf
Adedoyin <isradeoluwa@gmail.com>
2021-03-16 14:57:27 +0100

    New Screens Added

---
+++ lib/screens/generic_detail_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GenericDetailScreen extends StatefulWidget {
+  @override
+  _GenericDetailScreenState createState() => _GenericDetailScreenState();
+}
+
+class _GenericDetailScreenState extends State<GenericDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
