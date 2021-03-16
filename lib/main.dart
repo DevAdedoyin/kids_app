dfa4dde9a4f7cf287eaf93f72d117139331880b0
Adedoyin <isradeoluwa@gmail.com>
2021-03-16 15:02:26 +0100

    Named route added to each screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kids_learning_app/screens/alphabet_screen.dart';
+import 'package:kids_learning_app/screens/generic_detail_screen.dart';
+import './screens/main_screen.dart';
 
 void main() {
   runApp(KidsLearningApp());
@@ -10,7 +13,16 @@ class KidsLearningApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.pink, accentColor: Colors.orange),
+      title: 'Kids Learning (Offline)',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.orange,
+      ),
+      home: MainScreen(),
+      routes: {
+        GenericDetailScreen.routeName: (_) => GenericDetailScreen(),
+        AlphabetScreen.routeName: (_) => AlphabetScreen()
+      },
     );
   }
 }
