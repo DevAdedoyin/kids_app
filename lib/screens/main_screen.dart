21b41809bdc456b63065ad6ed810376398c40b37
Adedoyin <isradeoluwa@gmail.com>
2021-03-16 15:59:42 +0100

    Background Images added

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -7,29 +7,46 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Kids Learning (Offline)'),
-        actions: [
-          PopupMenuButton(
-              itemBuilder: (context) => [
-                    PopupMenuItem(
-                      child: Text('All Scores'),
-                    ),
-                    PopupMenuItem(
-                      child: Text('Settings'),
-                    ),
-                    PopupMenuItem(
-                      child: Text('About'),
-                    ),
-                    PopupMenuItem(
-                      child: Text('Share App'),
-                    ),
-                    PopupMenuItem(
-                      child: Text('More Apps'),
-                    ),
-                  ])
-        ],
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/bgimages/bgimg3.jpg'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text('Kids Learning (Offline)'),
+          actions: [
+            PopupMenuButton(
+                itemBuilder: (context) => [
+                      PopupMenuItem(
+                        child: Text('All Scores'),
+                      ),
+                      PopupMenuItem(
+                        child: Text('Settings'),
+                      ),
+                      PopupMenuItem(
+                        child: Text('About'),
+                      ),
+                      PopupMenuItem(
+                        child: Text('Share App'),
+                      ),
+                      PopupMenuItem(
+                        child: Text('More Apps'),
+                      ),
+                    ])
+          ],
+        ),
+        body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 5,
+            childAspectRatio: 3 / 4,
+          ),
+          itemBuilder: (context, index) {
+            return Card();
+          },
+        ),
       ),
     );
   }
